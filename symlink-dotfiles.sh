657ecc0908ef0d315306bdd58c89307de2c0fb9a
Toby Carvan <toby@redant.com.au>
2014-06-09 15:24:23 +1000

    wip

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/vim/.vim/ ~/.vim/
+ln -s ~/dotfiles/vim/.vim/ ~/.vim
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 rm -rf ~/.oh-my-zsh/custom
-ln -s ~/dotfiles/zsh/custom/ ~/.oh-my-zsh/custom/
+ln -s ~/dotfiles/zsh/custom/ ~/.oh-my-zsh/custom
