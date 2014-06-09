997735d0c337a6d86db375464d533a7c5161977f
Toby Carvan <toby@Tobys-MacBook-Pro.local>
2014-06-09 10:29:42 +1000

    fixed typo

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vim/.vim ~/.vim
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/oh-my-zsh/custom ~/.oh-my-zsh/custom
